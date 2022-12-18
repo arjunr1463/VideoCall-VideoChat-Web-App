93f3ba4e08c1401e2f0fa0e6b711729f03a84254
ARJUN RAMAKRISHNAN <71030433+arjunr1463@users.noreply.github.com>
2022-12-18 14:07:46 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import icon from "../assets/icon/videoicon.png";
 
 function navbar() {
   return (
-    <div className="bg-black h-[10vh] w-[100vw]">
+    <div className="bg-black h-[80px] w-[100vw]">
       <div className="flex items-center w-[100vw]">
         <img src={icon} alt="" className="w-30 h-[72px] px-2 lg:px-8 " />
         <h className="text-white font-extrabold  text-[18px] lg:text-[22px] lg:mb-2">WE-CALL</h>
